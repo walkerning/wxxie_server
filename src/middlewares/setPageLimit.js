f2b666c3f0c34b98f9a5452abc5bcee4a0cd835a
walkerning <ubuntu@shoephi2>
2018-05-25 17:29:34 +0800

    Fix stupid bug of setPageLimit...

--- src/middlewares/setPageLimit.js
+++ src/middlewares/setPageLimit.js
@@ -1,13 +1,16 @@
 var _ = require("lodash");
 
-module.exports = function setPageLimit(limit = 10, maxLimit = 100) {
-  return function(req, res, next) {
-    var query = req.query;
-    // default offset is `0`; default limit is `limit`
-    var offset = _.isInteger(query["offset"]) ? Math.max(query["offset"], 0) : 0;
-    var limit = _.isInteger(query["limit"]) ? Math.max(Max.min(query["limit"], maxLimit), 0) : limit;
-    req.query.offset = offset;
-    req.query.limit = limit;
-    next();
-  }
+module.exports = function setPageLimit(dlimit = 10, maxLimit = 100) {
+    return function(req, res, next) {
+	var query = req.query;
+	// default offset is `0`; default limit is `limit`
+	var query_offset = parseInt(query["offset"]);
+	var offset = isNaN(query_offset) ? 0 : Math.max(query_offset, 0);
+
+	var query_limit = parseInt(query["limit"]);
+	var limit = isNaN(query_limit) ? dlimit : Math.max(Math.min(query_limit, maxLimit), 0);
+	req.query.offset = offset;
+	req.query.limit = limit;
+	next();
+    }
 }
